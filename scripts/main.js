193db9e5aaa23fe2e2b22ed5a4e1132a17befa26
timdvd <76249593+timdvd@users.noreply.github.com>
2024-08-20 19:05:44 +0300

    commit

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* remove menu mobile */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* swiper */
+const swiper = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    gradCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0, -500],
+            rotate: [0, 0, 15],
+            // opacity: 0
+        },
+        next: {
+            translate: [100, 0, -500],
+            rotate: [0, 0, -15],
+            // opacity: 0
+        },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
+
+/* gsap */
+gsap.from('.home__images', 1.5, {opacity: 0, y: 150, delay: .2})
+gsap.from('.home__data', 1.8, {opacity: 0, x: -100, delay: .8})
+gsap.from('.home__info', 1.8, {opacity: 0, x: -100, delay: 1})
